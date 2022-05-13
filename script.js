6244a5f3431ef6d9d298e5ed1759172e4af7a0eb
MarcellaMendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-05-13 02:31:36 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.getElementById("count").innerText = 5
+
+let bonusPoints = 50
+bonusPoints = bonusPoints + 50
+bonusPoints = bonusPoints - 75
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+
